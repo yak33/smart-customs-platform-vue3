93bd4c1f460477547f7befdfc7972b35ee433f00
英明的张先生 <vuanne1@gmail.com>
2026-01-31 19:54:54 +0800

    feat(countries-search): 支持查询条件展开收起功能
    - 在国家搜索模块中新增展开/收起状态控制
    - 默认显示核心查询条件，展开后显示更多选项
    - 使用按钮切换展开状态，图标动态切换箭头方向
    - 优化查询表单布局，将操作按钮移动到第一行末尾
    - 新增键盘上下箭头图标的类型声明及引入

--- src/views/trade-compliance-library/countries/modules/countries-search.vue
+++ src/views/trade-compliance-library/countries/modules/countries-search.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, toRaw } from 'vue';
+import { computed, ref, toRaw } from 'vue';
 import { jsonClone } from '@sa/utils';
 import { useNaiveForm } from '@/hooks/common/form';
 import { $t } from '@/locales';
@@ -17,6 +17,9 @@ const emit = defineEmits<Emits>();
 const { validate, restoreValidation } = useNaiveForm();
 
 const model = defineModel<Api.TradeComplianceLibrary.CountriesSearchParams>('model', { required: true });
+
+// 展开/收起状态
+const expanded = ref(false);
 
 // 用于 Select 的字符串值
 const isenabledString = computed<'true' | 'false' | null>({
@@ -60,62 +63,65 @@ async function search() {
   await validate();
   emit('search');
 }
+
+function toggleExpand() {
+  expanded.value = !expanded.value;
+}
 </script>
 
 <template>
   <NCard :bordered="false" size="small" class="card-wrapper">
-    <NCollapse>
-      <NCollapseItem :title="$t('common.search')" name="business-countries-search">
-        <NForm :model="model" label-placement="left" :label-width="80">
-          <NGrid responsive="screen" item-responsive>
-            <NFormItemGi span="24 s:12 m:6" label="国家编码" label-width="auto" path="code" class="pr-24px">
-              <NInput v-model:value="model.code" placeholder="请输入国家编码" />
-            </NFormItemGi>
-            <NFormItemGi span="24 s:12 m:6" label="国家名称" label-width="auto" path="name" class="pr-24px">
-              <NInput v-model:value="model.name" placeholder="请输入国家名称" />
-            </NFormItemGi>
-            <NFormItemGi span="24 s:12 m:6" label="国家英文名称" label-width="auto" path="enname" class="pr-24px">
-              <NInput v-model:value="model.enname" placeholder="请输入国家英文名称" />
-            </NFormItemGi>
-            <NFormItemGi span="24 s:12 m:6" label="自定义代码" label-width="auto" path="standardCode" class="pr-24px">
-              <NInput v-model:value="model.standardCode" placeholder="请输入自定义代码" />
-            </NFormItemGi>
-            <NFormItemGi span="24 s:12 m:6" label="是否启用" label-width="auto" path="isenabled" class="pr-24px">
-              <NSelect
-                v-model:value="isenabledString"
-                placeholder="请选择是否启用"
-                :options="enableOptions"
-                clearable
-              />
-            </NFormItemGi>
-            <NFormItemGi span="24 s:12 m:6" label="预警级别" label-width="auto" path="warnLevel" class="pr-24px">
-              <NSelect
-                v-model:value="model.warnLevel"
-                placeholder="请选择预警级别"
-                :options="warnLevelOptions"
-                clearable
-              />
-            </NFormItemGi>
-            <NFormItemGi :show-feedback="false" span="24" class="pr-24px">
-              <NSpace class="w-full" justify="end">
-                <NButton @click="reset">
-                  <template #icon>
-                    <icon-ic-round-refresh class="text-icon" />
-                  </template>
-                  {{ $t('common.reset') }}
-                </NButton>
-                <NButton type="primary" ghost @click="search">
-                  <template #icon>
-                    <icon-ic-round-search class="text-icon" />
-                  </template>
-                  {{ $t('common.search') }}
-                </NButton>
-              </NSpace>
-            </NFormItemGi>
-          </NGrid>
-        </NForm>
-      </NCollapseItem>
-    </NCollapse>
+    <NForm :model="model" label-placement="left" :label-width="80">
+      <!-- 第一行：默认显示的查询条件 -->
+      <NGrid responsive="screen" item-responsive>
+        <NFormItemGi span="24 s:12 m:6" label="国家编码" label-width="auto" path="code" class="pr-24px">
+          <NInput v-model:value="model.code" placeholder="请输入国家编码" />
+        </NFormItemGi>
+        <NFormItemGi span="24 s:12 m:6" label="国家名称" label-width="auto" path="name" class="pr-24px">
+          <NInput v-model:value="model.name" placeholder="请输入国家名称" />
+        </NFormItemGi>
+        <NFormItemGi span="24 s:12 m:6" label="国家英文名称" label-width="auto" path="enname" class="pr-24px">
+          <NInput v-model:value="model.enname" placeholder="请输入国家英文名称" />
+        </NFormItemGi>
+        <!-- 操作按钮放在第一行末尾 -->
+        <NFormItemGi :show-feedback="false" span="24 s:12 m:6" class="pr-24px">
+          <NSpace class="w-full" justify="end" align="center">
+            <NButton @click="reset">
+              <template #icon>
+                <icon-ic-round-refresh class="text-icon" />
+              </template>
+              {{ $t('common.reset') }}
+            </NButton>
+            <NButton type="primary" ghost @click="search">
+              <template #icon>
+                <icon-ic-round-search class="text-icon" />
+              </template>
+              {{ $t('common.search') }}
+            </NButton>
+            <NButton text @click="toggleExpand">
+              {{ expanded ? '收起' : '展开' }}
+              <template #icon>
+                <icon-ic-round-keyboard-arrow-up v-if="expanded" class="text-icon" />
+                <icon-ic-round-keyboard-arrow-down v-else class="text-icon" />
+              </template>
+            </NButton>
+          </NSpace>
+        </NFormItemGi>
+      </NGrid>
+
+      <!-- 第二行：展开后显示的查询条件 -->
+      <NGrid v-if="expanded" responsive="screen" item-responsive>
+        <NFormItemGi span="24 s:12 m:6" label="自定义代码" label-width="auto" path="standardCode" class="pr-24px">
+          <NInput v-model:value="model.standardCode" placeholder="请输入自定义代码" />
+        </NFormItemGi>
+        <NFormItemGi span="24 s:12 m:6" label="是否启用" label-width="auto" path="isenabled" class="pr-24px">
+          <NSelect v-model:value="isenabledString" placeholder="请选择是否启用" :options="enableOptions" clearable />
+        </NFormItemGi>
+        <NFormItemGi span="24 s:12 m:6" label="预警级别" label-width="auto" path="warnLevel" class="pr-24px">
+          <NSelect v-model:value="model.warnLevel" placeholder="请选择预警级别" :options="warnLevelOptions" clearable />
+        </NFormItemGi>
+      </NGrid>
+    </NForm>
   </NCard>
 </template>
 
